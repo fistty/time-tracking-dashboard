69c9ecab0155859b2716ff84f56cb1452bf48e7d
Osuntuyi Amos <osuntuyiemilos@gmail.com>
2022-10-19 18:44:41 +0100

    Update DailyCard.jsx

--- src/components/DailyCard.jsx
+++ src/components/DailyCard.jsx
@@ -19,7 +19,7 @@ function DailyCard({ getSvg }) {
             <div className="info-container">
               <div className="title-container">
                 <p>{item.title}</p>
-                <svg width="21" height="5" xmlns="http://www.w3.org/2000/svg">
+                <svg className="elipsis" width="21" height="5" xmlns="http://www.w3.org/2000/svg">
                   <path
                     d="M2.5 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Zm8 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Zm8 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Z"
                     fill="#BBC0FF"
